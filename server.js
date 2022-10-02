0b461f69352fc3667a1ccb83dea2bb32d5e6420e
JJK70 <jjk701@gmail.com>
2022-10-02 14:52:13 -0400

    create functionality for profile page

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as beersRouter} from './routes/beers.js'
+import { router as profilesRouter } from './routes/profiles.js'
+
 
 // create the express app
 const app = express()
@@ -68,6 +70,8 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/beers', beersRouter)
+app.use('/profiles', profilesRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
